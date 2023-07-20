8c85a06f1fce593be6b44ae1a68a1b5df44b6420
Eazybel <eazybel27@gmail.com>
2023-07-20 04:58:40 -0700

    0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,8 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+#include <stdio.h>
 
-int main(void)
-{
- int i;
+void positive_or_negative(int i);
 
- i = 98;
- positive_or_negative(i);
-
- return (0);
-}
+#endif /* MAIN_H */
