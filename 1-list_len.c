b4615a188c292ca32e27ec5f9e8ace8686e57388
Eazybel <eazybel27@gmail.com>
2024-02-02 10:15:09 -0800

    started

---
+++ 1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list.
+ * @h: pointer to singly linked list.
+ *
+ * Return: number of elements.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
